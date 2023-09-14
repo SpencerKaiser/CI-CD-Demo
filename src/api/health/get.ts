92fb941ffd676380975f6d6ba65c1641ec3c94ad
Spencer Kaiser <spencer.kaiser@aa.com>
2023-09-14 08:47:12 -0500

    Adding an untested change

--- src/api/health/get.ts
+++ src/api/health/get.ts
@@ -1,8 +1,12 @@
 import { Request, Response } from 'express';
 
 export const get = async (req: Request, res: Response) => {
+  if (req.body.number > 0.5) {
+    console.log('greater than .5');
+  }
+
   res.status(200).send({
-    ok: false,
+    ok: true,
     hello: 'woirld',
   });
 };
--- tests/api/health/get.test.ts
+++ tests/api/health/get.test.ts
@@ -1,12 +1,15 @@
 import { get } from '../../../src/api/health/get';
 
+// const consoleLogSpy = jest.spyOn(console, 'log');
+
 describe('health GET', () => {
   it('returns a 200 status code and an ok status', () => {
-    const mockReq = {};
+    const mockReq = { body: { number: 0 } };
     const mockRes = { status: jest.fn().mockReturnThis(), send: jest.fn() };
 
     get(mockReq as any, mockRes as any);
     expect(mockRes.status).toBeCalledWith(200);
     expect(mockRes.send).toBeCalledWith(expect.objectContaining({ ok: true }));
+    // expect(consoleLogSpy).toHaveBeenCalledWith(expect.stringContaining('Random number'));
   });
 });
