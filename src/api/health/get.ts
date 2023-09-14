b2c278f02bb45d8b51f876d04259162be685750b
Spencer Kaiser <spencer.kaiser@aa.com>
2023-09-14 08:50:00 -0500

    Swapping to logger from console

--- src/api/health/get.ts
+++ src/api/health/get.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
+import { logger } from '../../utils/logger';
 
 export const get = async (req: Request, res: Response) => {
   if (req.body.number > 0.5) {
-    console.log('greater than .5');
+    logger.info('greater than .5');
   }
 
   res.status(200).send({
